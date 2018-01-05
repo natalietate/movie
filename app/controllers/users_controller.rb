7d949af59db68edefdc16aa5b66ee3011555eedd
Natalie Russo <martin.nat@gmail.com>
2018-01-05 14:05:35 -0500

    User show (#15)
    * create show page
    
    * add link to member's watchlist
    
    * if user clicks on themselves, go to full watchlist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,11 @@ class UsersController < ApplicationController
     @user = current_user
     @groups = Group.all
   end
+
+  def show
+    @user = User.find_by_id(params[:id])
+    if @user == current_user
+      redirect_to movies_watchlist_path
+    end
+  end
 end
